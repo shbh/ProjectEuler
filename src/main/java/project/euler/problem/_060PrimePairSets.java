924237a4f5259497369fd2710c3b502eafc8fefe
Shashi <shashib@adept-software.com>
2014-02-21 02:25:15 +0530

    60 solved

--- src/main/java/project/euler/problem/_060PrimePairSets.java
+++ src/main/java/project/euler/problem/_060PrimePairSets.java
@@ -1,104 +1,162 @@
-package project.euler.problem;
+ package project.euler.problem;  
+ 
+ import java.util.ArrayList;
+import java.util.HashMap;
 
-import static project.euler.feature.Functions.isPrime;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/*import java.util.ArrayList;
- import java.util.HashMap;
- import java.util.List;
- import java.util.Map;
- import java.util.Map.Entry;
- */
 import org.apache.log4j.Logger;
 
 import project.euler.IProblem;
-import project.euler.feature.NumberText;
+ public class _060PrimePairSets  implements IProblem 
+ {  
+      public static HashMap<Integer, Boolean> primes = new HashMap<Integer, Boolean>();  
+      public static HashMap<Integer, ArrayList<Integer>> ht_commons = new HashMap<Integer, ArrayList<Integer>>();  
+      
+      static Logger logger = Logger.getLogger(_060PrimePairSets.class);
 
-/*
- considering the given example is in three digit... so, maximum digit would be six digit... so, take out the combination till 999999...
-
- */
-public class _060PrimePairSets implements IProblem {
-
-	static Logger logger = Logger.getLogger(_060PrimePairSets.class);
-	// Map<Integer, List<Integer>> combinations = new HashMap<>();
-
-	int trail = 999999;
-	int marker = 999;
-
-	// List<String> primes = new ArrayList<>();
-	// List<Integer> numberundertrail = new ArrayList<>();
-
-	public Integer solve() {
-
-		int[] primes = new int[trail];
-		int[] numbers = new int[marker];
-
-		for (Integer i = 3; i < trail; i++) {
-
-			if (isPrime(i)) {
-				if (i < marker) {
-					numbers[i] = 1;
-				} 
-				primes[i]=1;
-			}
-		}
-
-		int[][] result = new int[marker][marker];
-
-		for (int i = 3; i < numbers.length; i = i + 2) {
-
-			if (numbers[i] == 1) {
-				for (int j = 3; j < numbers.length; j = j + 2) {
-					if (numbers[j] == 1) {
-						if (primes[Integer.parseInt(i + "" + j)] == 1) {
-							
-							result[i][j] = 1;
-
-						}
-					}
-				}
-			}
-		}
-
-		int solution = 0;
-		int sum = 0;
-		for (int i = 3; i < result.length; i=i+2) {// 3, 7, 109, and 673
-			int loop = 0;
-			
-			int totalsum = 0;
-			for (int j = 3; j < result[i].length; j=j+2) {// 3, 7, 109, and 673
-				loop =1;
-				if(result[i][j]==1) //7,9,673
-				{
-					for (int k = 3; k < result.length; k=k+2) {
-						if(result[j][k]==1 && result[i][k]==1 && result[k][i]==1) //7,9,673
-						{
-							loop=2;
-							for (int l = 3; l < result.length; l=l+2) {
-								loop =3;
-								if(result[k][l]==1 && result[i][l]==1 && result[l][i] == 1 && result[j][l]==1 && result[l][j]==1 ) //7,9,673
-								{
-									for (int m = 3; m < result.length; m=m+2) {
-										loop =3;
-										if(result[l][m]==1 && result[i][m]==1 && result[m][i] == 1 && result[j][m]==1 && result[m][j]==1 && result[k][m]==1 && result[m][k]==1 ) //7,9,673
-										{
-											logger.info(i+" "+j + " "+k + " "+l + " "+m);
-										}
-									}
-									
-								}
-							}
-						}
-					}
-				}
-			}
-
-		}
-
-		return 0;
-	}
-
-}
+      public Long solve()  
+      {  
+ 
+          
+    	  long sum = computeRemarkablePrimes(5);
+          
+    	  if(sum!=-1)  
+           {  
+                System.out.println("The computed sum is: " + sum);  
+           }  
+           else  
+           {  
+                System.out.println("Could not identify such prime numbers. Consider increasing the number of primes in your prime set");  
+           }  
+           
+           return sum;
+      }
+      
+      
+      /**  
+       * Computes the sum of the first <code>n</code> remarkable primes, where  
+       * two primes are defined as remarkable when the concatenation of the two  
+       * numbers creates a prime number as well. Example: 7 and 3.   
+       *   
+       * @param n  
+       * The number of desired remarkable primes.  
+       * @return  
+       * The sum of the first "n" remarkable primes   
+       */  
+      public static long computeRemarkablePrimes(int n)  
+      {  
+    	  long sum = 0;   
+          //Compute the primes and their pairwise remarkable primes between 2 and 10,000  
+          //if this fails to find the desired remarkable primes, consider increasing this number  
+          for(int i = 3; i <=10000; i=i+2 )  
+          {  
+               if(isPrime(i))  
+               {  
+                    ArrayList<Integer> al = new ArrayList<Integer>();  
+                    ht_commons.put(new Integer(i), al);  
+                    for(int j=i+2; j<=10000; ++j)  
+                    {  
+                         if(isPrime(j))  
+                         {  
+                              int p1 = Integer.parseInt(""+j+i);  
+                              int p2 = Integer.parseInt(""+i+j);  
+                              if(isPrime(p1) && isPrime(p2))  
+                              {  
+                                   al.add(new Integer(j));  
+                              }  
+                         }  
+                    }  
+               }  
+          }  
+          //Now we have our prime sets, iterate through the prime sets to find the n remarkable primes  
+          for(int i = 3; i <= 10000; i=i+2)  
+          {  
+               ArrayList<Integer> al1 = ht_commons.get(new Integer(i));  
+               ArrayList<Integer> answers = getMinimumMatch(al1, n-1);  
+               answers.add(new Integer(i));  
+               if(answers.size()==n)  
+               {  
+                    for(Integer answer: answers)  
+                    {  
+                         sum += answer.intValue();  
+                    }  
+                    break;  
+               }  
+          }
+		return sum; 
+      }  
+      
+      public static ArrayList<Integer> getMinimumMatch(ArrayList<Integer> matches, int size)  
+      {  
+           if(matches!=null && matches.size() >= size)  
+           {  
+                for(Integer parent: matches)  
+                {  
+                     ArrayList<Integer> al = ht_commons.get(parent);  
+                     if(al!=null && al.size()>=size)  
+                     {  
+                          ArrayList<Integer> test = new ArrayList<Integer>();  
+                          for(Integer child: al)  
+                          {  
+                               if(matches.contains(child))  
+                               {  
+                                    test.add(child);  
+                               }  
+                          }  
+                          if(test.size() >= (size)-1)//we got a match  
+                          {  
+                               if(size == 2)  
+                               {  
+                                    ArrayList<Integer> answer = new ArrayList<Integer>();  
+                                    answer.add(test.get(0));  
+                                    answer.add(parent);  
+                                    return answer;  
+                               }  
+                               ArrayList<Integer> answer = getMinimumMatch(test, size-1);  
+                               answer.add(parent);  
+                               if(answer.size() == size)  
+                               {  
+                                    return answer;  
+                               }  
+                          }  
+                     }  
+                }  
+           }  
+           return new ArrayList<Integer>();  
+      }  
+      /**  
+       * Checks whether a number is prime or not  
+       * @param num  
+       * @return  
+       */  
+      public static boolean isPrimeBrute(int num)  
+      {  
+           boolean is_prime = true;  
+           double sqrt = Math.sqrt(num);  
+           for(int i=2; i <= sqrt; ++i)  
+           {  
+                int mod = num % i;  
+                if(mod==0)  
+                {  
+                     is_prime = false;  
+                     break;  
+                }  
+           }  
+           return is_prime;  
+      }  
+      /**  
+       * Lazy load primes.  
+       * @param num  
+       * @return  
+       */  
+      public static boolean isPrime(int num)  
+      {  
+           Boolean is_prime = primes.get(new Integer(num));  
+           if(is_prime == null)  
+           {  
+                is_prime = new Boolean(isPrimeBrute(num));  
+                primes.put(new Integer(num), is_prime);  
+           }  
+           return is_prime.booleanValue();  
+      }  
+        
+ }  
