1be2a10aa0706a8e135d57ac91474773cf38ac9a
shbh <shashibb@gmail.com>
2014-12-31 15:27:48 +0530

    41

--- src/main/java/com/project/euler/launcher/LaunchPad.java
+++ src/main/java/com/project/euler/launcher/LaunchPad.java
@@ -19,7 +19,7 @@ public final class LaunchPad {
 		long lStartTime = System.nanoTime();
 
 
-		logger.info(new com.project.euler.problem._059XORDecryption().solve());
+		logger.info(new com.project.euler.problem._041PandigitalPrime().solve());
 
 		long lEndTime = System.nanoTime();
 		
--- src/main/java/com/project/euler/problem/_041PandigitalPrime.java
+++ src/main/java/com/project/euler/problem/_041PandigitalPrime.java
@@ -1,13 +1,15 @@
 package com.project.euler.problem;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
 import com.project.euler.IProblem;
 import com.project.euler.feature.Functions;
-import com.project.euler.feature.Permutations;
 
 public class _041PandigitalPrime implements IProblem {
 
@@ -23,7 +25,7 @@ public class _041PandigitalPrime implements IProblem {
 
 		for (int i = 9; i > 0; i--) {
 
-			Set<String> combinations = Permutations.generateCombination(Integer.toString(m));
+			Set<String> combinations = generateCombination(Integer.toString(m));
 
 			m = n / (int) Math.pow(10, i);
 
@@ -48,4 +50,69 @@ public class _041PandigitalPrime implements IProblem {
 		return pandigitalNumber;
 	}
 
+	
+	private static final int ZERO = 0;
+	
+	
+
+		// print N! permutation of the characters of the string s (in order)
+	public  void perm1(final String string, final Set<String> content) {
+		
+		perm1("", content, string);
+	
+	}
+
+	private  void perm1(final String prefix,
+			final Set<String> content, final String string) {
+		
+		final int nLength = string.length();
+		
+		if (nLength == ZERO) {
+		
+			content.add(prefix);
+	
+		}
+		else 
+		{
+		
+			for (int i = 0; i < nLength; i++) {
+			
+				perm1(prefix + string.charAt(i),
+						content,
+						string.substring(0, i)
+								+ string.substring(i + 1, nLength));
+			
+			}
+		}
+
+	}
+
+	
+
+	private  SortedSet<String> generateCombination(final String alphabet) {
+
+		return generateCombination(alphabet, alphabet.length());
+	
+	}
+
+	private  SortedSet<String> generateCombination(final String alphabet,
+			final int nLength) {
+
+		final SortedSet<String> strings = new TreeSet<String>(
+				new Comparator<String>() {
+	
+					public int compare(final String first, final String second) {
+						return Long.valueOf(second).compareTo(Long.valueOf(first));
+					
+					}
+
+				});
+		
+		final String elements = alphabet.substring(0, nLength);
+		
+		perm1(elements, strings);
+
+		return strings;
+	
+	}
 }
