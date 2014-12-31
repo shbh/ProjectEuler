9004defad3e77b41dc3a6869fcc2c3af88d0043b
shbh <shashibb@gmail.com>
2014-12-31 18:17:42 +0530

    exception

--- src/main/java/com/project/euler/feature/Reader.java
+++ src/main/java/com/project/euler/feature/Reader.java
@@ -14,86 +14,74 @@ import java.util.Properties;
 import com.project.euler.problem._054PokerHands;
 
 public class Reader {
-	
-	
+
 	private Map<Integer, String> results = new HashMap<Integer, String>();
 
 	public Reader() {
-	
+
 		this.readProperties();
-	
+
 	}
-	
-	
+
 	public Map<Integer, String> getResults() {
-	
+
 		return results;
-	
+
 	}
-	
-	public  Map<Integer, String> readProperties() {
-	
-		InputStream input = null;
-		
-		Properties props = new Properties();
-		
-		
+
+	public Map<Integer, String> readProperties() {
 
 		try {
+			InputStream input = null;
 
-			input = Reader.class.getClassLoader().getResourceAsStream(
-					"solution.properties");
+			Properties props = new Properties();
+
+			input = Reader.class.getClassLoader().getResourceAsStream("solution.properties");
 
 			// load a properties file from class path, inside static method
 			props.load(input);
 
 			props.load(input);
-		
+
 			Enumeration<Object> keys = props.keys();
 
 			while (keys.hasMoreElements()) {
-			
+
 				Object key = (Object) keys.nextElement();
-				
-				results.put(Integer.parseInt(key.toString()),
-						(String) props.get(key));
+
+				results.put(Integer.parseInt(key.toString()), (String) props.get(key));
 			}
 
-		} catch (IOException ex) {
-			
-			ex.printStackTrace();
-		
-		} finally {
-		
 			if (input != null) {
-			
+
 				try {
-				
+
 					input.close();
-				
+
 				} catch (IOException e) {
-				
+
 					e.printStackTrace();
-				
+
 				}
 			}
+
+		} catch (Exception ex) {
+
 		}
-		
 		return results;
-		
+
 	}
 
 	public void setResults(Map<Integer, String> results) {
 		this.results = results;
 	}
-	
-	public List<String> readFile(String fileName)
-	{
+
+	public List<String> readFile(String fileName) {
 
 		List<String> datas = new ArrayList<>(100);
-		
+
 		InputStream input = _054PokerHands.class.getClassLoader().getResourceAsStream(fileName);
-		
+
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(input))) {
 
 			String sCurrentLine;
@@ -103,13 +91,12 @@ public class Reader {
 				datas.add(sCurrentLine);
 			}
 
-		} 
-		catch (Exception e) {
-		
-		//	e.printStackTrace();
-		
+		} catch (Exception e) {
+
+			// e.printStackTrace();
+
 		}
-		
+
 		return datas;
 	}
 }
